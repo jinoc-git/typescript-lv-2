c13ed5c151c4c7d1da1c4f6993d2096efd327154
jinoc <choiae369@gmail.com>
2023-07-31 18:40:35 +0900

    Feat: redux, react-router-dom 추가

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,28 +1,14 @@
-import { useState } from 'react';
 import * as S from './style';
 import Form from '../form/Form';
-import shortid from 'shortid';
 import TodoList from '../todoList/TodoList';
-import Todo from '../../interfaces/Todo';
-
-// 자주 사용하는 Todo같은 interface는 따로 폴더로 분리해도 된다
-
-// initialTodo는 리렌더링 될 때마다 다시 선언하니 밖으로 빼자.
-const initialTodo: Todo = {
-  id: shortid.generate(),
-  title: '타입스크립트',
-  content: 'Lv1 과제 완성하기',
-  isDone: false,
-};
 
 const Main = () => {
-  const [todos, setTodos] = useState<Todo[]>([initialTodo]);
 
   return (
     <S.MainLayout>
-      <Form todos={todos} setTodos={setTodos} />
-      <TodoList todos={todos} setTodos={setTodos} isDone={false} />
-      <TodoList todos={todos} setTodos={setTodos} isDone={true} />
+      <Form />
+      <TodoList isDone={false} />
+      <TodoList isDone={true} />
     </S.MainLayout>
   );
 };
--- src/components/todoItem/TodoItem.tsx
+++ src/components/todoItem/TodoItem.tsx
@@ -1,27 +1,36 @@
 import * as S from './style';
 import Todo from '../../interfaces/Todo';
 import Button from '../common/button/Button';
+import { useDispatch } from 'react-redux';
+import { deleteTodo, switchTodo } from '../../redux/modules/todos';
 
 interface TodoItemProps {
   item: Todo;
-  isDone: boolean;
-  switchTodo: (id: string) => void;
-  deleteTodo: (id: string) => void;
 }
 
-const TodoItem = ({ item, isDone, switchTodo, deleteTodo }: TodoItemProps) => {
+const TodoItem = ({ item }: TodoItemProps) => {
+  const dispatch = useDispatch();
+
+  const onClickSwitchStatus = (id: string): void => {
+    dispatch(switchTodo(id));
+  };
+
+  const onClickDeleteTodo = (id: string): void => {
+    dispatch(deleteTodo(id));
+  };
+
   return (
     <S.TodoItemLayout>
       <S.TodoItemTitle>{item.title}</S.TodoItemTitle>
       <S.TodoItemContent>{item.content}</S.TodoItemContent>
       <S.ButtonBox>
-        <Button onClick={() => deleteTodo(item.id)} $bc={'#ff1616'}>
+        <Button onClick={() => onClickDeleteTodo(item.id)} $bc={'#ff1616'}>
           삭제하기
         </Button>
         <Button
-          $bc={isDone ? '#3c3030' : '#00b42e'}
-          onClick={() => switchTodo(item.id)}>
-          {isDone ? '취소하기' : '완료하기'}
+          $bc={item.isDone ? '#3c3030' : '#00b42e'}
+          onClick={() => onClickSwitchStatus(item.id)}>
+          {item.isDone ? '취소하기' : '완료하기'}
         </Button>
       </S.ButtonBox>
     </S.TodoItemLayout>
