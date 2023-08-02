3fa3772ce3bdebcb655bc7f5fa18fdde2113796c
jinoc <choiae369@gmail.com>
2023-08-02 12:26:16 +0900

    Refactor: toolkit과 axios, thunk 사용

--- src/redux/modules/todosSlice.ts
+++ src/redux/modules/todosSlice.ts
@@ -1,88 +1,92 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import axios, { AxiosRequestConfig, AxiosResponse, AxiosResponseHeaders } from 'axios';
+import { isAxiosError } from 'axios';
 import Todo from '../../interfaces/Todo';
-import api from // , { getTodos, addTodo, switchTodo, deleteTodo }
-'../../axios/api';
+import api from '../../axios/api';
 
-// interface ErrorType {
-//   errMessage: string
-// }
+interface ErrorType {
+  message: string;
+}
 
-// interface AxiosError<T = any, D = any> extends Error {
-//   config: AxiosRequestConfig<D>;
-//   code?: string;
-//   request?: any;
-//   response?: AxiosResponse<T, D>;
-//   isAxiosError: boolean;
-//   toJSON: () => object;
-// }
+interface ResponseDataType {
+  message: string;
+  code: string;
+}
 
-// interface ResponseDataType {
-//   message: string;
-//   code: number;
-// }
-
-export const __getTodos = createAsyncThunk(
-  'getTodos',
-  async (_, thunkAPI) => {
-    try {
-      const { data } = await api.get<Todo[]>('/todos');
-      return thunkAPI.fulfillWithValue(data);
-    } catch (error) {
-      console.log(error);
-      return thunkAPI.rejectWithValue(error);
+export const __getTodos = createAsyncThunk<
+  Todo[],
+  undefined,
+  { rejectValue: ErrorType }
+>('getTodos', async (_, thunkAPI) => {
+  try {
+    const { data } = await api.get<Todo[]>('/todos');
+    return thunkAPI.fulfillWithValue(data);
+  } catch (error) {
+    if (isAxiosError<ResponseDataType>(error)) {
+      return thunkAPI.rejectWithValue({ message: error.message });
+    } else {
+      return thunkAPI.rejectWithValue({ message: '알 수 없는 오류 발생' });
     }
   }
-);
+});
 
-export const __addTodo = createAsyncThunk(
-  'addTodo',
-  async (payload: Todo, thunkAPI) => {
-    try {
-      await api.post('/todos', payload);
-      const { data } = await api.get<Todo[]>('/todos');
-      return thunkAPI.fulfillWithValue(data);
-    } catch (error) {
-      console.log(error);
-      return thunkAPI.rejectWithValue(error);
+export const __addTodo = createAsyncThunk<
+  Todo,
+  Todo,
+  { rejectValue: ErrorType }
+>('addTodo', async (payload: Todo, thunkAPI) => {
+  try {
+    const res = await api.post<Todo>('/todos', payload);
+    return thunkAPI.fulfillWithValue(res.data);
+  } catch (error) {
+    if (isAxiosError<ResponseDataType>(error)) {
+      return thunkAPI.rejectWithValue({ message: error.message });
+    } else {
+      return thunkAPI.rejectWithValue({ message: '알 수 없는 오류 발생' });
     }
   }
-);
+});
 
-export const __switchTodo = createAsyncThunk(
-  'switchTodo',
-  async (payload: Todo, thunkAPI) => {
-    try {
-      const switchedTodo: Todo = { ...payload, isDone: !payload.isDone };
-      await api.patch(`/todos/${switchedTodo.id}`, switchedTodo);
-      const { data } = await api.get<Todo[]>('/todos');
-      return thunkAPI.fulfillWithValue(data);
-    } catch (error) {
-      console.log(error);
-      return thunkAPI.rejectWithValue(error);
+export const __switchTodo = createAsyncThunk<
+  Todo,
+  Todo,
+  { rejectValue: ErrorType }
+>('switchTodo', async (payload: Todo, thunkAPI) => {
+  try {
+    const switchedTodo: Todo = { ...payload, isDone: !payload.isDone };
+    const res = await api.patch(`/todos/${switchedTodo.id}`, switchedTodo);
+    return thunkAPI.fulfillWithValue(res.data);
+  } catch (error) {
+    if (isAxiosError<ResponseDataType>(error)) {
+      return thunkAPI.rejectWithValue({ message: error.message });
+    } else {
+      return thunkAPI.rejectWithValue({ message: '알 수 없는 오류 발생' });
     }
   }
-);
+});
 
-export const __deleteTodo = createAsyncThunk(
-  'deleteTodo',
-  async (payload: string, thunkAPI) => {
-    try {
-      await api.delete(`/todos/${payload}`);
-      const { data } = await api.get<Todo[]>('/todos');
-      return thunkAPI.fulfillWithValue(data);
-    } catch (error) {
-      console.log(error);
-      return thunkAPI.rejectWithValue(error);
+export const __deleteTodo = createAsyncThunk<
+  Todo[],
+  string,
+  { rejectValue: ErrorType }
+>('deleteTodo', async (payload: string, thunkAPI) => {
+  try {
+    await api.delete(`/todos/${payload}`);
+    const { data } = await api.get<Todo[]>('/todos');
+    return thunkAPI.fulfillWithValue(data);
+  } catch (error) {
+    if (isAxiosError<ResponseDataType>(error)) {
+      return thunkAPI.rejectWithValue({ message: error.message });
+    } else {
+      return thunkAPI.rejectWithValue({ message: '알 수 없는 오류 발생' });
     }
   }
-);
+});
 
 interface State {
   todos: Todo[];
   isLoading: boolean;
   isError: boolean;
-  error: unknown;
+  error: unknown | string;
 }
 
 const initialState: State = {
@@ -109,7 +113,7 @@ export const todosSlice = createSlice({
     builder.addCase(__getTodos.rejected, (state, action) => {
       state.isLoading = false;
       state.isError = true;
-      state.error = action.error;
+      state.error = action.error.message;
     });
     builder.addCase(__addTodo.pending, (state, action) => {
       state.isLoading = true;
@@ -118,12 +122,12 @@ export const todosSlice = createSlice({
     builder.addCase(__addTodo.fulfilled, (state, action) => {
       state.isLoading = false;
       state.isError = false;
-      state.todos = action.payload;
+      state.todos.push(action.payload);
     });
     builder.addCase(__addTodo.rejected, (state, action) => {
       state.isLoading = false;
       state.isError = true;
-      state.error = action.error;
+      state.error = action.error.message;
     });
     builder.addCase(__switchTodo.pending, (state, action) => {
       state.isLoading = true;
@@ -132,12 +136,17 @@ export const todosSlice = createSlice({
     builder.addCase(__switchTodo.fulfilled, (state, action) => {
       state.isLoading = false;
       state.isError = false;
-      state.todos = action.payload;
+      state.todos = state.todos.map((item) => {
+        if (item.id === action.payload.id) {
+          item.isDone = action.payload.isDone;
+        }
+        return item;
+      });
     });
     builder.addCase(__switchTodo.rejected, (state, action) => {
       state.isLoading = false;
       state.isError = true;
-      state.error = action.error;
+      state.error = action.error.message;
     });
     builder.addCase(__deleteTodo.pending, (state, action) => {
       state.isLoading = true;
@@ -151,7 +160,7 @@ export const todosSlice = createSlice({
     builder.addCase(__deleteTodo.rejected, (state, action) => {
       state.isLoading = false;
       state.isError = true;
-      state.error = action.error;
+      state.error = action.error.message;
     });
   },
 });
